e631faa7478ca528eb6fc108a7b4148e156c9807
TarasovVP <vptarasov1998@gmail.com>
2025-01-18 13:30:21 +0200

    Added architecture_template_shared as a submodule

--- shared/src/commonMain/kotlin/com/vnteam/architecturetemplateswebapp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.vnteam.architecturetemplateswebapp
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
